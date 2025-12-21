10e238ad8606c812dbf63ba23b20a7f37e47eff9
Namgel <namgeltamang14@gmail.com>
2025-12-21 20:11:51 +0545

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+import "./App.css";
+
+function App() {
+    return <Link to="http://localhost:8000/api/v1/auth/google">Google</Link>;
+}
+
+export default App;
