2fb9a383f38e5fbceb9be118e97fab3d82d4e99e
Namgel <namgeltamang14@gmail.com>
2025-12-21 20:43:40 +0545

    Initialize tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
+import App from "@/app/App.tsx";
+import "@/index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
-import "./index.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            <QueryClientProvider client={queryClient}>
+                <App />
+                <ReactQueryDevtools />
+            </QueryClientProvider>
         </BrowserRouter>
     </StrictMode>
 );
