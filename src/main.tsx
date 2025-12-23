5abdbb296522be1c24dc2fdc0675523275af271d
Namgel <namgeltamang14@gmail.com>
2025-12-23 22:09:54 +0545

    Configure react hot toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import App from "@/app/App.tsx";
-import "@/index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "@/app/App.tsx";
+
+import "@/index.css";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +17,12 @@ createRoot(document.getElementById("root")!).render(
         <BrowserRouter>
             <QueryClientProvider client={queryClient}>
                 <App />
+                <Toaster
+                    position="top-center"
+                    toastOptions={{
+                        duration: 2000,
+                    }}
+                />
                 <ReactQueryDevtools />
             </QueryClientProvider>
         </BrowserRouter>
