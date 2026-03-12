be839d609625fc809c4b189270ecc947f567c021
Namgel <namgeltamang14@gmail.com>
2026-03-12 22:05:35 +0545

    Remove generic response interface

--- src/features/auth/api/index.ts
+++ src/features/auth/api/index.ts
@@ -1,6 +1,5 @@
 import { API_ENDPOINT } from "@/shared/constants/api.constants";
 import api from "@/shared/lib/api";
-import type { Response } from "@/shared/types/response";
 import type { LoginResponse } from "../types/login";
 import type {
     RegisterEmailFormFields,
@@ -11,28 +10,19 @@ import type {
 const authApi = {
     // Sign up - register email
     registerEmail: async (data: RegisterEmailFormFields) => {
-        const response = await api.post<Response<void>>(
-            API_ENDPOINT.auth.registerEmail,
-            data,
-        );
+        const response = await api.post(API_ENDPOINT.auth.registerEmail, data);
         return response.data;
     },
 
     // Sign up - verify email
     verifyEmail: async (data: VerifyEmailFormFields) => {
-        const response = await api.post<Response<void>>(
-            API_ENDPOINT.auth.verifyEmail,
-            data,
-        );
+        const response = await api.post(API_ENDPOINT.auth.verifyEmail, data);
         return response.data;
     },
 
     // Sign up - register user
     registerUser: async (data: RegisterUserFormFields) => {
-        const response = await api.post<Response<void>>(
-            API_ENDPOINT.auth.register,
-            data,
-        );
+        const response = await api.post(API_ENDPOINT.auth.register, data);
         return response.data;
     },
 
@@ -47,9 +37,7 @@ const authApi = {
 
     // Logout
     logout: async () => {
-        const response = await api.post<Response<void>>(
-            API_ENDPOINT.auth.logout,
-        );
+        const response = await api.post(API_ENDPOINT.auth.logout);
         return response.data;
     },
 };
--- src/features/auth/hooks/useVerifyEmail.ts
+++ src/features/auth/hooks/useVerifyEmail.ts
@@ -1,7 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 
 import type { ApiError } from "@/shared/types/api-error";
-import type { Response } from "@/shared/types/response";
 import { handleErrorResponse } from "@/shared/utils/handleErrorResponse";
 import toast from "react-hot-toast";
 import authApi from "../api";
@@ -10,7 +9,7 @@ import type { VerifyEmailFormFields } from "../validation";
 // Sign up - verify email
 export const useVerifyEmail = () => {
     const { mutateAsync: verifyEmailMutation, isPending: isLoading } =
-        useMutation<Response<void>, ApiError, VerifyEmailFormFields>({
+        useMutation<void, ApiError, VerifyEmailFormFields>({
             mutationFn: authApi.verifyEmail,
             onSuccess: () => {
                 toast.success("Email verified successfully");
