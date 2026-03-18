3680cb96b21c1b14672d6db5fc8befb824c1c64c
Namgel <namgeltamang14@gmail.com>
2026-03-18 06:33:17 +0545

    Refactor auth logic

--- src/shared/lib/api.ts
+++ src/shared/lib/api.ts
@@ -3,9 +3,38 @@ import axios, {
     type InternalAxiosRequestConfig,
 } from "axios";
 
+import { API_ENDPOINT } from "../constants/api.constants";
+import type { LoginResponse } from "@/features/auth/types/login";
+
 export const API_URL = import.meta.env.VITE_PUBLIC_API_URL;
 
 const api = axios.create({ baseURL: API_URL, withCredentials: true });
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config as AxiosRequestConfigWithRetry;
+
+        if (originalRequest.url?.includes(API_ENDPOINT.auth.refreshToken)) {
+            return Promise.reject(error);
+        }
+
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                await api.post<LoginResponse>(API_ENDPOINT.auth.refreshToken);
+
+                return api(originalRequest);
+            } catch (refreshError) {
+                localStorage.removeItem("user");
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(error);
+    },
+);
 
 export default api;
 
@@ -13,7 +42,6 @@ export interface AxiosRequestConfigWithRetry extends AxiosRequestConfig {
     _retry?: boolean;
 }
 
-export interface InternalAxiosRequestConfigWithRetry
-    extends InternalAxiosRequestConfig {
+export interface InternalAxiosRequestConfigWithRetry extends InternalAxiosRequestConfig {
     _retry?: boolean;
 }
