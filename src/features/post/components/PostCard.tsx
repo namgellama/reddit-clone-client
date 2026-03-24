66c47fb9f70035dc3db325d712a6aa7e53cd3825
Namgel <namgeltamang14@gmail.com>
2026-03-24 19:52:27 +0545

    Build reusable vote controls component

--- src/features/post/components/PostCard.tsx
+++ src/features/post/components/PostCard.tsx
@@ -1,7 +1,8 @@
-import { ArrowBigDown, ArrowBigUp, MessageCircle } from "lucide-react";
+import { MessageCircle } from "lucide-react";
 
 import { useAuth } from "@/contexts/AuthContext";
 import { useTogglePostVote } from "@/features/vote/hooks/useTogglePostVote";
+import { VoteControls } from "@/shared/components/common";
 import { Avatar, AvatarFallback } from "@/shared/components/ui/avatar";
 import { Button } from "@/shared/components/ui/button";
 import { timeAgo } from "@/shared/utils/timeAgo";
@@ -58,37 +59,16 @@ const PostCardFooter = ({ post }: { post: Post }) => {
 
     return (
         <div className="flex items-center gap-3">
-            <div
-                className={`w-fit h-8 p-1 ${post.user_vote === "UPVOTE" ? "bg-primary text-white" : post.user_vote === "DOWNVOTE" ? "bg-secondary text-white" : "bg-gray-300/65 text-black"} rounded-full flex items-center justify-center gap-2`}
-            >
-                <Button
-                    disabled={isLoading || !isAuthenticated}
-                    variant="ghost"
-                    className={`size-6 p-0 hover:bg-inherit ${post.user_vote !== null ? "hover:text-white" : "hover:text-primary"}`}
-                    onClick={() => togglePostVoteMutation({ type: "UPVOTE" })}
-                >
-                    {
-                        <ArrowBigUp
-                            className="size-[1.15rem]"
-                            fill={
-                                post.user_vote === "UPVOTE" ? "white" : "none"
-                            }
-                        />
-                    }
-                </Button>
-                {post.score}
-                <Button
-                    disabled={isLoading || !isAuthenticated}
-                    variant="ghost"
-                    className={`size-6 p-0 hover:bg-inherit ${post.user_vote !== null ? "hover:text-white" : "hover:text-secondary"}`}
-                    onClick={() => togglePostVoteMutation({ type: "DOWNVOTE" })}
-                >
-                    <ArrowBigDown
-                        className="size-[1.15rem] "
-                        fill={post.user_vote === "DOWNVOTE" ? "white" : "none"}
-                    />
-                </Button>
-            </div>
+            <VoteControls
+                disabled={isLoading || !isAuthenticated}
+                score={post.score}
+                userVote={post.user_vote}
+                upvoteOnClick={() => togglePostVoteMutation({ type: "UPVOTE" })}
+                downvoteOnClick={() =>
+                    togglePostVoteMutation({ type: "DOWNVOTE" })
+                }
+            />
+
             <Button className="w-fit h-8 bg-gray-300/65 rounded-full flex items-center gap-2  text-black hover:bg-gray-300/80">
                 <MessageCircle className="size-4" />
                 {post.comment_count}
