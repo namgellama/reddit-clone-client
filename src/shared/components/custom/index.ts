41a6c3b197f5d2aa2a0ac21425c3e206ee78c530
Namgel <namgeltamang14@gmail.com>
2026-03-25 13:33:07 +0545

    Build custom avatar component

--- src/shared/components/custom/index.ts
+++ src/shared/components/custom/index.ts
@@ -1,3 +1,4 @@
+export { default as Avatar } from "./Avatar";
 export { default as Dot } from "./Dot";
 export { default as Form } from "./Form";
 export { default as FormTextInput } from "./FormTextInput";
