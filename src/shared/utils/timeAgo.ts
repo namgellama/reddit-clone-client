2ddc122f62424c1eb80c53631d73c69b3d249fbe
Namgel <namgeltamang14@gmail.com>
2026-03-25 13:26:09 +0545

    Fix value format

--- src/shared/utils/timeAgo.ts
+++ src/shared/utils/timeAgo.ts
@@ -4,29 +4,29 @@ export function timeAgo(date: Date | string | number): string {
     const diff = Math.floor((now - past) / 1000); // seconds
 
     if (diff < 60) {
-        return `${diff} second${diff !== 1 ? "s" : ""} ago`;
+        return `${diff}s ago`;
     }
 
     const minutes = Math.floor(diff / 60);
     if (minutes < 60) {
-        return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
+        return `${minutes}m ago`;
     }
 
     const hours = Math.floor(minutes / 60);
     if (hours < 24) {
-        return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
+        return `${hours}h ago`;
     }
 
     const days = Math.floor(hours / 24);
     if (days < 30) {
-        return `${days} day${days !== 1 ? "s" : ""} ago`;
+        return `${days}d ago`;
     }
 
     const months = Math.floor(days / 30);
     if (months < 12) {
-        return `${months} month${months !== 1 ? "s" : ""} ago`;
+        return `${months}m ago`;
     }
 
     const years = Math.floor(months / 12);
-    return `${years} year${years !== 1 ? "s" : ""} ago`;
+    return `${years}y ago`;
 }
